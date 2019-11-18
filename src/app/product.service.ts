f1b3bfc557ee26ee275a33c4d88e2d86dced2b07
ethan.hansen <eth.hansen@gmail.com>
2019-11-18 12:18:39 -0500

    mes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,8 +4,10 @@ import {Http, Response} from '@angular/http'
 @Injectable()
 export class ProductService {
 
+  priavte _albumUrl = '../assets/album.json'
+
   constructor(private _http: Http) {
-    const _albumUrl = '../assets/album.json'
+
   }
 
   getAlbum(id: number){
