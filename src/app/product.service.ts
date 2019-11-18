8a3a29414a2252aef8fcac468bc40397e8b4e968
ethan.hansen <eth.hansen@gmail.com>
2019-11-18 12:41:00 -0500

    mes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,8 +12,10 @@ export class ProductService {
 
   }
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => {
+      return response.json();
+    });
   }
 
 }
